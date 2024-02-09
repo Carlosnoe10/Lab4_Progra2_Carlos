7bb9ddb501c4fa109509f420eb08532c0272b187
Carlosnoe10 <carlosdnoem@gmail.com>
2024-02-09 11:06:38 -0800

    Primer commit

---
+++ src/lab4_prograii_carlos/Lab4_PrograII_Carlos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab4_prograii_carlos;
+
+/**
+ *
+ * @author cd507
+ */
+public class Lab4_PrograII_Carlos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
