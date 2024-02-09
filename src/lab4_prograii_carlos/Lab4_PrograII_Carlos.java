226b9d4dfdffd8a3e4db71f5f0ba2a87bf0f2794
CarlosArnulfo <carlosarnulfo@unitec.edu>
2024-02-09 14:13:17 -0600

    menu

--- src/lab4_prograii_carlos/Lab4_PrograII_Carlos.java
+++ src/lab4_prograii_carlos/Lab4_PrograII_Carlos.java
@@ -4,6 +4,8 @@
  */
 package lab4_prograii_carlos;
 
+import java.util.Scanner;
+
 /**
  *
  * @author cd507
@@ -14,9 +16,32 @@ public class Lab4_PrograII_Carlos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("a");
-        System.out.println("a");
-        System.out.println("aaaa");
+        Scanner sc = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+       
+        int menu;
+        do{
+
+                    System.out.println("1: Ejercicio 1");
+                    System.out.println("2: Ejercicio 2");
+                    System.out.println("3: Ejercicio 3");
+                    System.out.println("4: Salir");
+                    menu = sc.nextInt();
+            switch (menu){
+                case (1):
+
+                    break;
+                case (2):
+
+                    break;
+                case (3):
+                    
+                    break;
+                default:
+            }
+                    
+        }   while (menu!=0);
+        System.out.println("Saliendo del programa");
     }
     
 }
