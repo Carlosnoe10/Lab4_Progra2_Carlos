7cf935c94f4c596f7642b9cd5ff36ef8cc94dd1e
CarlosArnulfo <carlosarnulfo@unitec.edu>
2024-02-09 14:46:42 -0600

    push main 2

--- src/lab4_prograii_carlos/Lab4_PrograII_Carlos.java
+++ src/lab4_prograii_carlos/Lab4_PrograII_Carlos.java
@@ -18,21 +18,21 @@ public class Lab4_PrograII_Carlos {
      */
     public static void main(String[] args) {
         ArrayList <Personas> personas=new ArrayList();
+        personas.add(new Usuario("odioariotgames@gmail.com","SuTilin","mantecahn",5.65));
+        personas.add(new Usuario("britanycarrasco@gmail.com","SuTilina","awadeuwu",500.00));
         Scanner sc = new Scanner(System.in);
         Scanner input = new Scanner(System.in);
        
         int menu;
         do{
             System.out.println("Inicio de sesion");
-            System.out.println("Ingrese su nombre de usuario");
+            System.out.println("Ingrese su nombre de usuario o correo electronico");
+            int usuario=verificarUsuario(input.nextLine(),personas,0);
             String name=input.nextLine();
-            try{
-                
-            }catch(Exception e){
-                System.out.println("");
-        }finally{
-                
+            if (usuario==1) {
+                System.out.println("inicio de sesio con exito");
             }
+            
                     System.out.println("1: Ejercicio 1");
                     System.out.println("2: Ejercicio 2");
                     System.out.println("3: Ejercicio 3");
@@ -46,13 +46,24 @@ public class Lab4_PrograII_Carlos {
 
                     break;
                 case (3):
-                    
+
                     break;
                 default:
             }
-                    
-        }   while (menu!=0);
+
+        } while (menu != 0);
         System.out.println("Saliendo del programa");
     }
-    
+
+    private static int verificarUsuario(String inicioSesion, ArrayList<Personas> usuarios, int i) {
+        if (inicioSesion.equals(((Usuario) usuarios.get(i)).getCorreo()) || inicioSesion.equals(((Usuario) usuarios.get(i)).getNombreDeUsu())) {
+            return i;
+        } else if (i <= usuarios.size()) {
+            i=verificarUsuario(inicioSesion, usuarios, i + 1);
+        } else {
+
+        }
+        return i;
+    }
+
 }
